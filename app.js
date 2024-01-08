7de12ef11de58216a563f36f41075ee6724e8422
Roopesh Sai <127601942+Roopesh2004@users.noreply.github.com>
2024-01-08 18:39:58 +0530

    Update app.js

--- app.js
+++ app.js
@@ -95,24 +95,14 @@ function speakThis(message) {
         speech.text = finalText;
 
     } else if (message.includes('my mode')) {
-        setTimeout(function() {
-            window.open("https://leetcode.com/problemset/", "_blank");
-        }, 1000); // Delay in milliseconds
+        window.open("https://leetcode.com/problemset/", "_blank");
+        window.open("https://www.youtube.com/", "_blank");
+        window.open("https://chat.openai.com/", "_blank");
+        window.open("https://web.whatsapp.com/", "_blank");
 
-        setTimeout(function() {
-            window.open("https://www.youtube.com/", "_blank");
-        }, 2000);
-
-        setTimeout(function() {
-            window.open("https://chat.openai.com/", "_blank");
-        }, 3000);
-
-        setTimeout(function() {
-            window.open("https://web.whatsapp.com/", "_blank");
-        }, 4000);
-
-        const finalText = "Opening my mode     all the best for your coding";
+        const finalText = "Opening my mode";
         speech.text = finalText;
+
     } else {
         window.open(`https://www.google.com/search?q=${message.replace(" ", "+")}`, "_blank");
         const finalText = "I found some information for " + message + " on google";
